ec3ed11d8df71bf6b3a5e99048acc0bb3bb90409
rto24 <richardto180@gmail.com>
2024-11-14 16:39:56 -0500

    changed file structure and added Login component

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+import LoginPage from "@/components/Login";
+
+export default function Login() {
+  return (
+    <LoginPage />
+  );
+}
---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+  <>
+    <div>Login</div>
+    <button className="px-12 py-4 rounded-full bg-[#1ED760] font-bold text-white tracking-widest uppercase transform hover:scale-105 hover:bg-[#21e065] transition-colors duration-200">
+      Login
+    </button>
+  </>
+  )
+}
+
+export default LoginPage
