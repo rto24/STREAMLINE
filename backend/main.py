f956a571b22fac6d97ad9f351843d58fcb5f3493
rto24 <richardto180@gmail.com>
2024-11-14 16:39:24 -0500

    removed database connection test

--- backend/main.py
+++ backend/main.py
@@ -14,20 +14,3 @@ app.include_router(auth_router, prefix="/auth")
 @app.get("/")
 async def root():
   return {"message": "Welcome to the Spotify Authentication App"}
-  
-@app.get("/ping-supabase")
-async def ping_supabase():
-  try:
-    # Make a basic request to Supabase (fetches settings as a quick check)
-    response = supabase.table("people").select("*").limit(1).execute()
-    print(response)
-    return {"message": f"{response}"}
-    # Check if there is an error in the response
-    if response.error:
-      raise HTTPException(status_code=500, detail=f"Supabase error: {response.error.message}")
-      # raise BaseException()
-    
-      return {"message": "Supabase connection successful", "data": response}
-  except Exception as e:
-    raise HTTPException(status_code=500, detail=f"Failed to connect to Supabase: {str(e)}")
-    # raise BaseException()
