82ad7d64c3673b3ec8e3ee71414cf2ecf5c07a4d
rto24 <richardto180@gmail.com>
2024-11-15 16:47:11 -0500

    added user context to wrap app and get spotify username globally

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react'
+import { UserContextInterface } from '@/types/types'
+
+const UserContext = createContext<UserContextInterface>({
+  username: null,
+  setUsername: () => {},
+});
+
+const UserProvider = ({ children }: {children: ReactNode}) => {
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/auth/user", {
+          credentials: "include",
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setUsername(data.user.sub);
+        } 
+      } catch (error) {
+        console.error('Failed to get user:', error);
+      }
+    }
+    fetchUser();
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ username, setUsername }}>
+      { children }
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within UserProvider");
+  }
+  return context;
+}
+
+export default UserProvider
