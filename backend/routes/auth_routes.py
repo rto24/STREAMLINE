40784b692f54113a83334fa40ca27f9cf7121f1d
rto24 <richardto180@gmail.com>
2024-11-18 17:26:13 -0500

    post user data to database

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Query, Response, Depends
 from backend.controllers.auth_controller import generate_spotify_login_url, handle_spotify_callback
 from backend.middleware.auth_middleware import jwt_middleware_config
+from backend.db.supabase_client import get_user_by_username, insert_user
 from fastapi.responses import RedirectResponse
 
 
@@ -15,7 +16,15 @@ async def login():
 async def callback(code: str):
   try:
     data = await handle_spotify_callback(code)
+    print("Spotify callback data:", data["profile_data"]["display_name"])
     jwt_token = data["jwt_token"]
+    
+    username = data["profile_data"]["display_name"]
+    playlists = []
+    
+    existing_user = get_user_by_username(username)
+    if not existing_user:
+      insert_user(username, playlists)
     
     frontend_url = "http://localhost:3000/home"
     response = RedirectResponse(url=f"{frontend_url}")
