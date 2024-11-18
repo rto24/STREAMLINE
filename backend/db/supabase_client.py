f8d2dd4b90e653339ca30996cc76a580641480cd
rto24 <richardto180@gmail.com>
2024-11-18 17:26:40 -0500

    added methods to post user data to supabase

--- backend/db/supabase_client.py
+++ backend/db/supabase_client.py
@@ -6,3 +6,20 @@ import os
 load_dotenv()
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def get_user_by_username(username: str):
+  response = supabase.table("person").select("id").eq("username", username).execute()
+  print("get_user_by_username response:", response)
+  if not response:
+    raise Exception(f"Error fetching user: {response['error']}")
+  return response.data
+
+def insert_user(username: str, playlists: list):
+  print(f"Inserting user: {username}, playlists: {playlists}")
+  response = supabase.table("person").insert({
+    "username": username,
+    "playlists": playlists
+  }).execute()
+  if not response:
+    raise Exception(f"Error inserting user: {response['error']}")
+  return response.data
