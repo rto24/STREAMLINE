16159ad455fb04c101868eb5a91fa53954ce6033
rto24 <richardto180@gmail.com>
2024-11-22 17:16:38 -0500

    added route to cleaned up spotify data

--- backend/routes/spotify_data_routes.py
+++ backend/routes/spotify_data_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Query, Response, Depends
 from backend.middleware.auth_middleware import jwt_middleware_access_token
 from backend.controllers.spotify_data_controller import get_user_top_tracks, get_user_top_artists, get_user_audio_metadata
+from backend.scripts.fetch_data import fetch_user_data
 
 router = APIRouter()
 
@@ -26,5 +27,13 @@ async def user_audio_metadata(access_token: str = Depends(jwt_middleware_access_
   try:
     audio_metadata = get_user_audio_metadata(access_token)
     return {"audio metadata": audio_metadata}
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/organized-data")
+async def fetch_organized_data(access_token: str = Depends(jwt_middleware_access_token)):
+  try:
+    fetched_data = fetch_user_data(access_token)
+    return {"user data": fetched_data}
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
