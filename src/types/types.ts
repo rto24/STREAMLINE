03b3e5dc02911310277a407b59bf1317bc25f8f8
rto24 <richardto180@gmail.com>
2024-11-25 22:46:26 -0500

    interface for songs

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,11 @@ export interface UserContextInterface {
   username: string | null,
   setUsername: React.Dispatch<React.SetStateAction<string | null>>;
 }
+
+export interface GeneratedSongInterface {
+  id: string,
+  name: string,
+  desc: string,
+  img: string,
+  album: string
+}
