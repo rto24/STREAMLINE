8cef4b6ea07ce3dfecfba7ddedb50498041d2c0a
rto24 <richardto180@gmail.com>
2024-12-03 18:47:25 -0500

    realigned and rendered new ui components

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FlipWords } from './ui/word-flip';
+import { flipWords } from '@/data/data';
+import { WavyBackground } from './ui/wavy-bg';
 
 const LoginPage = () => {
   const handleLogin = async () => {
@@ -16,16 +19,29 @@ const LoginPage = () => {
   }
 
   return (
-  <>
-    <div>Login</div>
-    <button 
-      className="px-12 py-4 rounded-full bg-[#1ED760] font-bold text-white tracking-widest uppercase transform hover:scale-105 hover:bg-[#21e065] transition-colors duration-200"
-      onClick={handleLogin}
-    >
-      Login
-    </button>
-  </>
-  )
+    <>
+      <WavyBackground>
+        <div className="flex flex-col items-center justify-center min-h-screen w-full text-center px-4">
+          <div className="mb-6 text-white text-9xl font-bold">
+            STREAMLINE
+          </div>
+          <div className="text-4xl text-center mb-5 font-normal text-white whitespace-nowrap">
+            Music that is more <br></br>
+            <FlipWords 
+              words={flipWords}
+              className="text-green-300 font-bold"
+            />
+          </div>
+          <button 
+            className="px-12 py-4 mb-8 rounded-full bg-[#1ED760] font-bold text-white tracking-widest uppercase transform hover:scale-105 hover:bg-[#21e065] transition-colors duration-200"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
+      </WavyBackground>
+    </>
+  );
 }
 
 export default LoginPage
