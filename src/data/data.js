71a58ed57b24858c2d7eb001ac44a3bdb360a25d
rto24 <richardto180@gmail.com>
2024-12-04 18:12:27 -0500

    added data for typewriter ui

--- src/data/data.js
+++ src/data/data.js
@@ -14,3 +14,42 @@ export const loadingText = [
 ]
 
 export const flipWords = ['personal', 'accurate', 'meaningful', 'exciting', 'memorable']
+
+export const typewriterWords = [
+  {
+    text: "Start",
+    className: "text-white"
+  },
+  {
+    text: "generating",
+    className: "text-white"
+  },
+  {
+    text: "songs",
+    className: "text-white"
+  },
+  {
+    text: "with",
+    className: "text-white"
+  },
+  {
+    text: "STREAMLINE",
+    className: "text-green-300"
+  },
+  {
+    text: "in",
+    className: "text-white"
+  },
+  {
+    text: "a",
+    className: "text-white"
+  },
+  {
+    text: "single",
+    className: "text-white"
+  },
+  {
+    text: "click.",
+    className: "text-white"
+  },
+]
