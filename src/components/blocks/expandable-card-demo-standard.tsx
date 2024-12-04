2ad02fac55bb2917c4e584b3f973ad8308861ff2
rto24 <richardto180@gmail.com>
2024-12-04 18:37:02 -0500

    added border on hover for generated songs

--- src/components/blocks/expandable-card-demo-standard.tsx
+++ src/components/blocks/expandable-card-demo-standard.tsx
@@ -52,8 +52,8 @@ export default function ExpandableCardDemo({ cards }: ExpandableCardDemoProps) {
               <motion.div layoutId={`image-${active.name}-${id}`}>
                 <Image
                   priority
-                  width={200}
-                  height={200}
+                  width={1000}
+                  height={1000}
                   src={active.img}
                   alt={active.name}
                   className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
@@ -91,19 +91,19 @@ export default function ExpandableCardDemo({ cards }: ExpandableCardDemoProps) {
         )}
       </AnimatePresence>
 
-      <ul className="max-w-2xl mx-auto w-full gap-4">
+      <ul className="max-w-2xl mx-auto w-full gap-4 mt-2">
         {cards.map((card, idx) => (
           <motion.div
             key={`${card.name}${idx}`}
             layoutId={`card-${card.name}-${id}`}
             onClick={() => setActive(card)}
-            className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+            className="p-4 flex flex-col md:flex-row justify-between items-center border-2 border-black hover:border-green-300 rounded-xl cursor-pointer dark:hover:bg-neutral-800"
           >
             <div className="flex gap-4 flex-col md:flex-row">
               <motion.div layoutId={`image-${card.name}-${id}`}>
                 <Image
-                  width={100}
-                  height={100}
+                  width={300}
+                  height={300}
                   src={card.img}
                   alt={card.name}
                   className="h-40 w-40 md:h-14 md:w-14 rounded-lg object-cover object-top"
@@ -112,7 +112,7 @@ export default function ExpandableCardDemo({ cards }: ExpandableCardDemoProps) {
               <div>
                 <motion.h3
                   layoutId={`title-${card.name}-${id}`}
-                  className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left"
+                  className="font-medium text-white dark:text-neutral-200 text-center md:text-left"
                 >
                   {card.name}
                 </motion.h3>
