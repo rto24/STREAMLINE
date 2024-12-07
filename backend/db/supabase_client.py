5447fc2545aff16327b23d3a3d816064122ec499
rto24 <richardto180@gmail.com>
2024-12-06 20:17:46 -0500

    added method to update database

--- backend/db/supabase_client.py
+++ backend/db/supabase_client.py
@@ -8,11 +8,11 @@ load_dotenv()
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def get_user_by_username(username: str):
-  response = supabase.table("person").select("id").eq("username", username).execute()
+  response = supabase.table("person").select("id", "playlists").eq("username", username).execute()
   print("get_user_by_username response:", response)
   if not response:
     raise Exception(f"Error fetching user: {response['error']}")
-  return response.data
+  return response.data[0]
 
 def insert_user(username: str, playlists: list):
   print(f"Inserting user: {username}, playlists: {playlists}")
@@ -22,4 +22,10 @@ def insert_user(username: str, playlists: list):
   }).execute()
   if not response:
     raise Exception(f"Error inserting user: {response['error']}")
+  return response.data
+
+def update_user_playlist(username: str, playlists: list):
+  response = supabase.table("person").update({"playlists": playlists}).eq("username", username).execute()
+  if not response:
+    raise Exception(f"Error updating playlist: {response['error']}")
   return response.data
