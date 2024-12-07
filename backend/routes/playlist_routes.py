56d28cfcb0affe483ba8037054d13324a50b2e13
rto24 <richardto180@gmail.com>
2024-12-06 20:17:11 -0500

    added route for playlist post

---
+++ backend/routes/playlist_routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException
+from backend.middleware.auth_middleware import jwt_middleware_access_token
+from backend.db.supabase_client import update_user_playlist, get_user_by_username
+from backend.models.playlist import PlaylistRequestBody
+from backend.controllers.playlist_controller import save_playlist_controller
+
+router = APIRouter()
+
+@router.post("/{username}")
+async def save_song_to_user_playlist(username: str, body: PlaylistRequestBody):
+  try:
+    return await save_playlist_controller(username, body)
+  except Exception as e:
+    print("Error in playlist route:", str(e))
+    raise HTTPException(status_code=500, detail=str(e))
+    
