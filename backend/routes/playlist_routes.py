68c849222943fe1df4d9cdf65c5623d8f1b46dec
rto24 <richardto180@gmail.com>
2024-12-08 11:46:57 -0500

    added route to get user saved songs

--- backend/routes/playlist_routes.py
+++ backend/routes/playlist_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from backend.middleware.auth_middleware import jwt_middleware_access_token
-from backend.db.supabase_client import update_user_playlist, get_user_by_username
+from backend.db.supabase_client import update_user_playlist, get_user_by_username, get_user_playlist
 from backend.models.playlist import PlaylistRequestBody
 from backend.controllers.playlist_controller import save_playlist_controller
 
@@ -13,4 +13,7 @@ async def save_song_to_user_playlist(username: str, body: PlaylistRequestBody):
   except Exception as e:
     print("Error in playlist route:", str(e))
     raise HTTPException(status_code=500, detail=str(e))
-    
+
+@router.get("/{username}")
+def get_user_saved_songs(username: str):
+  return get_user_playlist(username)
