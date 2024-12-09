4cef1dc217fd02a836b87e063b5eae54170ee449
rto24 <richardto180@gmail.com>
2024-12-09 16:47:13 -0500

    shows saved songs

--- src/components/SavedSongs.tsx
+++ src/components/SavedSongs.tsx
@@ -2,11 +2,14 @@
 
 import React, { useState } from 'react'
 import { SavedSongInterface, PlaylistProps } from '@/types/types'
+import ExpandableCardDemo from './blocks/expandable-card-demo-standard'
 
 const SavedSongs = ({ initialItems }: PlaylistProps) => {
   const [ savedSongs, setSavedSongs ] = useState<SavedSongInterface[]>(initialItems)
+  console.log(savedSongs)
   return (
-    <div>SavedSongs</div>
+    //change method to have actual logic later
+    <ExpandableCardDemo saveToPlaylist={() => console.log("Hello World")} cards={savedSongs} />
   )
 }
 
